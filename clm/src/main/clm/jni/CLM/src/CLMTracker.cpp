bcae39b9c7a2f3f1661d6c1a61a5be7295e9a9e4
Manuel Martin <manuel.martin@wearebase.com>
2016-01-13 17:39:47 +0000

    Add log error to CLMTracker when face is not detected

--- clm/src/main/clm/jni/CLM/src/CLMTracker.cpp
+++ clm/src/main/clm/jni/CLM/src/CLMTracker.cpp
@@ -2,7 +2,7 @@
 // Copyright (C) 2014, University of Southern California and University of Cambridge,
 // all rights reserved.
 //
-// THIS SOFTWARE IS PROVIDED “AS IS” FOR ACADEMIC USE ONLY AND ANY EXPRESS
+// THIS SOFTWARE IS PROVIDED "AS IS" FOR ACADEMIC USE ONLY AND ANY EXPRESS
 // OR IMPLIED WARRANTIES WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 // THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
 // PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
@@ -15,13 +15,13 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //
 // Notwithstanding the license granted herein, Licensee acknowledges that certain components
-// of the Software may be covered by so-called “open source” software licenses (“Open Source
-// Components”), which means any software licenses approved as open source licenses by the
+// of the Software may be covered by so-called "open source" software licenses ("Open Source
+// Components"), which means any software licenses approved as open source licenses by the
 // Open Source Initiative or any substantially similar licenses, including without limitation any
 // license that, as a condition of distribution of the software licensed under such license,
 // requires that the distributor make the software available in source code format. Licensor shall
 // provide a list of Open Source Components for a particular version of the Software upon
-// Licensee’s request. Licensee will comply with the applicable terms of such licenses and to
+// Licensee's request. Licensee will comply with the applicable terms of such licenses and to
 // the extent required by the licenses covering Open Source Components, the terms of such
 // licenses will apply in lieu of the terms of this Agreement. To the extent the terms of the
 // licenses applicable to Open Source Components prohibit any of the restrictions in this
@@ -49,6 +49,11 @@
 #include "stdafx.h"
 
 #include <CLMTracker.h>
+
+#include <android/log.h>
+
+#define LOG_TAG "CLMTracker-JNI"
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 using namespace CLMTracker;
 using namespace cv;
@@ -480,7 +485,7 @@ bool CLMTracker::DetectLandmarksInImage(const Mat_<uchar> &grayscale_image, cons
 
 		// calculate the local and global parameters from the generated 2D shape (mapping from the 2D to 3D because camera params are unknown)
 		clm_model.pdm.CalcParams(clm_model.params_global, bounding_box, clm_model.params_local, rotation_hypotheses[hypothesis]);
-	
+
 		bool success = clm_model.DetectLandmarks(grayscale_image, depth_image, params);	
 
 		if(hypothesis == 0 || best_likelihood < clm_model.model_likelihood)
@@ -531,6 +536,7 @@ bool CLMTracker::DetectLandmarksInImage(const Mat_<uchar> &grayscale_image, cons
 
 	if(bounding_box.width == 0)
 	{
+		LOGE("No face detected");
 		return false;
 	}
 	else
